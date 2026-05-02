8456970ba30d84e2bb9725dd5ff2c5edef3b9996
anji65 <anjivarun305@gmail.com>
2026-05-03 00:12:10 +0530

    init database

--- src/app.js
+++ src/app.js
@@ -1,27 +1,32 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
+const User = require("./models/user");
 
-app.use("/admin", adminAuth);
+app.post("/signup", async (req, res) => {
+  try {
+    const user = new User({
+      firstName: "varun",
+      lastName: "tej",
+      emailId: "varun@example.com",
+      password: "varun@123",
+    });
 
-app.get("/user", userAuth, (req, res) => {
-  res.send("User data sent!");
+    await user.save();
+    res.send("User created successfully");
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user" });
+  }
 });
 
-app.post("/user/login", (req, res) => {
-  res.send("User logged in successfully!");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("All data sent!");
-});
-
-app.delete("/admin/deleteUser", (req, res) => {
-  res.send("User deleted!");
-});
-
-app.listen(3000, () => {
-  console.log("Server is successfully running on port 3000....");
-});
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully!!");
+    app.listen(3000, () => {
+      console.log("Server is successfully running on port 3000....");
+    });
+  })
+  .catch((err) => {
+    console.error("Database can't be connected!!");
+  });
